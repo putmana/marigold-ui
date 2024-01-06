63067e7bb4c27bfe6f43d48ac08bee76f8ac826a
putmana <adam@ptmn.io>
2024-01-06 05:40:47 -0700

    Began work on UI library

---
+++ src/lib/components/button/button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	
+</script>
+
+<button> 
+	Test
+</button>
+
+<style lang="scss">
+	@use "../vars";
+
+	button {
+		background-color: vars.$color_button;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '/static/style/global.scss';
+	import Button from "$lib/components/button/button.svelte";
+
+</script>
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button />
